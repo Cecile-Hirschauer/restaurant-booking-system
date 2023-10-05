ce34c94081539479569eedb22c1071e2658b0e83
cecile hirschauer <cecile.h.75@gmail.com>
2023-10-05 18:21:23 +0200

    Add name and capacity to restaurant

--- src/entities/restaurant.ts
+++ src/entities/restaurant.ts
@@ -1,1 +1,28 @@
-export class Restaurant {}
+export class Restaurant {
+    private _name: string | undefined;
+    private _capacity: number | undefined;
+
+    constructor(name: string, capacity: number) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    get name(): string | undefined {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get capacity(): number | undefined {
+        return this._capacity;
+    }
+
+    set capacity(value: number) {
+        if (value <= 0) {
+            throw new Error('Capacity must be greater than 0');
+        }
+        this._capacity = value;
+    }
+}
--- tests/entities/restaurant.test.ts
+++ tests/entities/restaurant.test.ts
@@ -1,8 +1,15 @@
 import { Restaurant } from '../../src/entities/restaurant';
 
 describe('Restaurant', () => {
-    it('Should exist', () => {
-       const restaurant = new Restaurant();
+    it('Should exist and have a name', () => {
+       const restaurant = new Restaurant('Sushi Place', 10);
          expect(restaurant).toBeTruthy();
+         expect(restaurant.name).toEqual('Sushi Place');
     });
-});
+    it('Should not allow a negative capacity', () => {
+        expect(() => {
+            const restaurant = new Restaurant('Chez Pierre', -5);
+        }).toThrowError('Capacity must be greater than 0');
+    });
+
+  });
